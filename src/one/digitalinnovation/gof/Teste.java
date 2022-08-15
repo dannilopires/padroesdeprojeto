2fd07c4d90a587d3ccec5f8f0a47ca3f93761a8c
dannilo <dannilo-pires@hotmail.com>
2022-08-15 11:38:33 -0300

    implementa exemplos de singleton e strategy

---
+++ src/one/digitalinnovation/gof/Teste.java
@@ -0,0 +1,64 @@
+package one.digitalinnovation.gof;
+
+import one.digitalinnovation.gof.singleton.SingletonEager;
+import one.digitalinnovation.gof.singleton.SingletonLazy;
+import one.digitalinnovation.gof.singleton.SingletonLazyHolder;
+import one.digitalinnovation.gof.strategy.Comportamento;
+import one.digitalinnovation.gof.strategy.ComportamentoAgressivo;
+import one.digitalinnovation.gof.strategy.ComportamentoDefensivo;
+import one.digitalinnovation.gof.strategy.ComportamentoNormal;
+import one.digitalinnovation.gof.strategy.Robo;
+
+public class Teste {
+	
+	public static void main(String[] args) {
+		
+		//Singleton
+		SingletonLazy lazy = SingletonLazy.getInstancia();
+		System.out.println(lazy);
+		lazy = SingletonLazy.getInstancia();
+		System.out.println(lazy);
+		
+		SingletonEager eager = SingletonEager.getInstancia();
+		System.out.println(eager);
+		eager = SingletonEager.getInstancia();
+		System.out.println(eager);
+		
+		SingletonLazyHolder holder = SingletonLazyHolder.getInstancia();
+		System.out.println(holder);
+		holder = SingletonLazyHolder.getInstancia();
+		System.out.println(holder);
+		
+		
+		// Strategy
+		Comportamento normal = new ComportamentoNormal();
+		Comportamento defensivo = new ComportamentoDefensivo();
+		Comportamento agressivo = new ComportamentoAgressivo();
+		
+		Robo robo = new Robo();
+		robo.setComportamento(normal);
+		
+		System.out.println(" ");
+		
+		robo.mover();
+		robo.mover();
+		
+		robo.setComportamento(defensivo);
+		
+		System.out.println(" ");
+		
+		robo.mover();
+		
+        robo.setComportamento(agressivo);
+        
+        System.out.println(" ");
+		
+		robo.mover();
+		robo.mover();
+		robo.mover();
+		
+		
+		
+	}
+
+}
