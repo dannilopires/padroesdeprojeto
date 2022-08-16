43e4f22ae1a8b786ee1f461dc3a1c4cd1b98d3fb
dannilo <dannilo-pires@hotmail.com>
2022-08-16 11:09:56 -0300

    implementa exemplo de facade

--- src/one/digitalinnovation/gof/Teste.java
+++ src/one/digitalinnovation/gof/Teste.java
@@ -1,5 +1,6 @@
 package one.digitalinnovation.gof;
 
+import one.digitalinnovation.gof.facade.Facade;
 import one.digitalinnovation.gof.singleton.SingletonEager;
 import one.digitalinnovation.gof.singleton.SingletonLazy;
 import one.digitalinnovation.gof.singleton.SingletonLazyHolder;
@@ -56,6 +57,11 @@ public class Teste {
 		robo.mover();
 		robo.mover();
 		robo.mover();
+		
+		//Facade
+		
+		Facade facade = new Facade();
+		facade.migrarCliente("Dannilo", "3452989");
 		
 		
 		
